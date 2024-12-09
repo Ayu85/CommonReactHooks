f5ed9aa3e2f30fe416876e7db654559c2a22d494
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-12-09 03:27:21 +0000

    useCouter hook impl.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
+import useCounter from "./components/customHooks/useCounter";
 import FocusInput from "./components/useRef/Case1";
 import Chat from "./components/useRef/Case2";
 import Stopwatch from "./components/useRef/Case3";
 
 export default function App() {
+  const {value,increment,decrement}=useCounter()
   return (
     <main>
       {/* <FocusInput /> */}
       {/* <Chat/> */}
-      <Stopwatch/>
+      {/* <Stopwatch/> */}
+      {value}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </main>
   );
 }
