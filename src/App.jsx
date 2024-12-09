0f810c6928d0fcfffe40ceabccb92ede7a161e91
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-12-09 02:50:27 +0000

    use cases of ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import FocusInput from "./components/useRef/Case1";
+import Chat from "./components/useRef/Case2";
+import Stopwatch from "./components/useRef/Case3";
 
 export default function App() {
   return (
     <main>
-      <FocusInput />
+      {/* <FocusInput /> */}
+      {/* <Chat/> */}
+      <Stopwatch/>
     </main>
   );
 }
